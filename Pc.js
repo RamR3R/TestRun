14578e051a63eec394bea40e1c7d2e4e4af8ae88
RamR3R <sendtoramsundar@gmail.com>
2022-12-24 17:23:35 +0530

    file created from Pc

---
+++ Pc.js
@@ -0,0 +1,16 @@
+let a = "Luffy";
+let b = "zoro";
+let c = "zoro";
+
+if (a == b) {
+  if (a == c)
+    console.log("a,b,c are Equal");
+  else
+    console.log("a,b are Equal");
+}
+else {
+  if (a == c)
+    console.log("a,c are Equal");
+  else
+    console.log("All are Different");
+}
